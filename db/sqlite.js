cc2892c31ed17807c52e590a0b515089929340fd
Mike Matera <maximus@fatboycentral.com>
2021-03-29 13:34:34 -0700

    re set db file.

--- db/sqlite.js
+++ db/sqlite.js
@@ -8,7 +8,7 @@ var _db = null;
 async function check_db() {
     if (_sqlite3 == null) {
         _sqlite3 = require('sqlite-async')
-        _db = await _sqlite3.open(':memory:')
+        _db = await _sqlite3.open('/tmp/db.sqlite')
     }
     return _db
 }
